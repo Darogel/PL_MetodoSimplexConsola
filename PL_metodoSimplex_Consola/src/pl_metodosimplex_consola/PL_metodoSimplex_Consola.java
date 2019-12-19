458902ab325811f09bef4f620abf08736570fbf5
darogel <41120975+darogel@users.noreply.github.com>
2019-12-19 13:04:12 -0500

    Actualización
    Se ha corregido errores de ortografía, así como tambien llamada de metodos

--- PL_metodoSimplex_Consola/src/pl_metodosimplex_consola/PL_metodoSimplex_Consola.java
+++ PL_metodoSimplex_Consola/src/pl_metodosimplex_consola/PL_metodoSimplex_Consola.java
@@ -20,21 +20,19 @@ public class PL_metodoSimplex_Consola {
     //Objetos Utilizados para llenar Variables de clase atributoFunción
     public static atributoOperacion funcionObj = new atributoOperacion();
     public static atributoOperacion restriccion = new atributoOperacion();
+    public static boolean opcionFuncion = false;
 
     //ArrayList de la clase atributosFunción
     public static ArrayList<atributoOperacion> funcion = new ArrayList<>();
 
     public static void main(String[] args) {
 
-        //Variables para utilizar en modelo
+        operacion opera = new operacion();
+
         String cad = " UNIVERSIDAD NACIONAL DE LOJA \n"
                 + "          INGENIERÍA EN SISTEMAS \n"
                 + "               MÉTODO SIMPLEX \n"
                 + "Ingresar cantidad variables: ";
-        //Variable utilizada para llamar metodos de Operaiòn
-        operacion opera = new operacion();
-        // TODO code application logic here
-        Scanner reload = new Scanner(System.in);
 
         int intVar = Integer.parseInt(JOptionPane.showInputDialog(null, cad));
         JOptionPane.showMessageDialog(null, "Ingrese Valores de Función Objetiva");
@@ -42,9 +40,13 @@ public class PL_metodoSimplex_Consola {
         for (int i = 0; i < intVar; i++) {
             Double doValor = Double.parseDouble(JOptionPane.showInputDialog(null,
                     "Ingrese valor Variable X" + (i + 1) + ": "));
-            //se llenan los valores de las variables
             funcionObj.variables.add(doValor);
         }
+        funcionObj.condicion = 1;
+        int intIncog = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese \n"
+                + "Termino independiente: "));
+
+        funcionObj.valorIndependiente = Double.parseDouble(String.valueOf(intIncog));
         funcion.add(funcionObj);
         //Ingreso de RESTRICCIONES
         int intRest = Integer.parseInt(JOptionPane.showInputDialog(null,
@@ -53,39 +55,33 @@ public class PL_metodoSimplex_Consola {
         for (int i = 0; i < intRest; i++) {
             restriccion = new atributoOperacion();
             JOptionPane.showMessageDialog(null, "Restricción: " + (i + 1));
-            //Se ingresan las variables de cada restricción
             for (int j = 0; j < intVar; j++) {
                 Double doValor = Double.parseDouble(JOptionPane.showInputDialog(null,
-                        "Ingrese valor Variable X" + (i + 1) + ": "));
+                        "Ingrese valor Variable X" + (j + 1) + ": "));
                 restriccion.variables.add(doValor);
             }
-            //Se selecciona la condición de la función
             int intCon = JOptionPane.showOptionDialog(null, "Seleccione el operador",
-                            "Ingresar funcion", JOptionPane.YES_NO_CANCEL_OPTION,
-                            JOptionPane.QUESTION_MESSAGE, null, 
-                            new Object[]{"≤", "=", "≥"}, "≥");
-            
+                    "Ingresar funcion", JOptionPane.YES_NO_CANCEL_OPTION,
+                    JOptionPane.QUESTION_MESSAGE, null,
+                    new Object[]{"≤", "=", "≥"}, "≥");
+
             restriccion.condicion = intCon;
-            //se ingresa valor de termino independiente
             Double doValor = Double.parseDouble(JOptionPane.showInputDialog(null,
-                        "Ingrese termino independiente: "));
+                    "Ingrese termino independiente: "));
             restriccion.valorIndependiente = doValor;
             funcion.add(restriccion);
         }
-        
+
         int intMaxmin = Integer.parseInt(JOptionPane.showInputDialog(null,
                 "Ingrese Tipo de Operación \n"
-                        + "1. Maximizar \n"
-                        + "2. Minimizar "));
+                + "1. Maximizar \n"
+                + "2. Minimizar "));
         if (intMaxmin == 1) {
-            opera.presentarFuncionObj();
-            System.out.println("");
-            System.out.println("Funión Maximizada");
-            System.out.println("");
             opera.estandarizarFuncion();
             opera.pivoteo();
         } else {
-            opera.presentarFuncionObj();
+            opcionFuncion = true;
+            opera.estandarizarFuncion();
             opera.pivoteo();
         }
 
